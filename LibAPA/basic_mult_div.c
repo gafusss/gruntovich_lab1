76f6cdeafb08c029d9b3711142b2d0393f319e67
gafusss <A.gafusss.A@gmail.com>
2016-11-22 20:45:34 +0300

    Commented apa_mult

--- LibAPA/basic_mult_div.c
+++ LibAPA/basic_mult_div.c
@@ -9,21 +9,36 @@
 // c - произведение a и b, размером size_a + size_b слов
 void apa_mult(uint16_t* a, uintmax_t size_a, uint16_t* b, uintmax_t size_b, uint16_t* c)
 {
-	// TODO: Comment
+	// Слово переноса разряда при умножении
 	uint16_t d;
+
+	//Результат элементарного умножения слов
 	uint32_t T;
+
+	//Размер числа c
 	uintmax_t size_c = size_a + size_b;
+
+	//Обнуление результата
 	apa_fill(c, size_c, 0);
 
 	for (uintmax_t i = 0; i < size_a; i++)
 	{
+		//Обнуление переноса
 		d = 0;
+
 		for (uintmax_t j = 0; j < size_b; j++)
 		{
+			//Элементарное умножение слов чисел с переносом
 			T = (uint32_t)c[i + j] + (uint32_t)a[i] * (uint32_t)b[j] + (uint32_t)d;
+
+			//Сохранение полученного при умножении слова
 			c[i + j] = LOWORD(T);
+
+			//Перенос разряда
 			d = HIWORD(T);
 		}
+
+		//Сохранение переноса в следующий разряд
 		c[i + size_b] = d;
 	}
 }
